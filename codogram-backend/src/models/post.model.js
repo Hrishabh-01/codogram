9ac489494644ae8fd6e9322bd0e60a0062a03f84
Hrishabh Agrawal <hrishabhagrawal122@gmail.com>
2025-02-11 11:02:20 +0530

    added bcrypt,jwt for password hashing and token generation.Also added JWT tokens secret and expiry in .env. Also added some methods and hooks in user model

--- codogram-backend/src/models/post.model.js
+++ codogram-backend/src/models/post.model.js
@@ -1,9 +1,10 @@
 import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
 
 const postSchema=new Schema(
     {
         user:{
-            type: mongoose.Schema.Types.ObjectId,
+            type: Schema.Types.ObjectId,
             ref: "User", // Links the post to its creator
             required: true
         },
@@ -20,12 +21,16 @@ const postSchema=new Schema(
             type: String, // Array of media URLs (images/videos)
             required: true
         }],
+        duration:{
+            type:Number,
+            default:0
+        },
         codeSnippet: {
             language: { type: String, trim: true }, // e.g., "JavaScript", "Python"
             content: { type: String, trim: true }, // The actual code snippet
         },
         likes: [{
-            type: mongoose.Schema.Types.ObjectId,
+            type: Schema.Types.ObjectId,
             ref: "User" // Stores user IDs who liked the post
         }],
         comments: [{
@@ -40,8 +45,8 @@ const postSchema=new Schema(
             type: Boolean,
             default: true // Public post by default
         }
-        
-
     },{timestamps:true})
+
+postSchema.plugin(mongooseAggregatePaginate)
 
 export const Post=mongoose.model("Post",postSchema)
