3c8044e6debe1c0ef717ef0bf6be686c8f1187a3
Hrishabh Agrawal <hrishabhagrawal122@gmail.com>
2025-02-12 14:19:24 +0530

    feat: implement user registration controller with avatar upload and validation

--- codogram-backend/src/controllers/user.controller.js
+++ codogram-backend/src/controllers/user.controller.js
@@ -1,9 +1,81 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js";
+import {User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
 
 const registerUser = asyncHandler(async (req,res)=>{
-    return res.status(200).json({
-        message:"ok"
+    //get user deatail from frontend
+    //validation- not empty
+    //check if user already exists:username , email
+    //check for images
+    //check for avatar
+    //upload them to cloudinary,avatar
+    //create user object - create entry in db
+    //remove password and refresh token field from response
+    //check for user creation 
+    //return res
+
+    const {fullname,email,username,password,bio,skills}=req.body
+    console.log("email : ",email);
+
+    if(
+        [fullname,email,username,password].some((field)=>
+            field?.trim()==="")
+    ){
+        throw new ApiError(400,"All fields are required !!!")
+    }
+
+    const existedUser = User.findOne({
+        $or:[{ username },{ email }]
     })
+    console.log("existedUser : ",existedUser);
+
+    if(existedUser){
+        throw new ApiError(409,"User already exists")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    console.log("avatarLocalPath : ",avatarLocalPath);
+
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar is required!!!")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    console.log("avatar : ",avatar);  
+    if(!avatar){
+        throw new ApiError(500,"Failed to upload avatar")
+    }
+
+    const user=await User.create({
+        fullname,
+        avatar:avatar.url,
+        email,
+        password,
+        username:username.toLowerCase(),
+        bio,
+        skills
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new ApiError(500,"Failed to register user!!!")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(
+            200,
+            createdUser,
+            "User registered successfully"
+        )
+    )
+
+
 })
 
 export {registerUser}
--- codogram-backend/src/models/user.model.js
+++ codogram-backend/src/models/user.model.js
@@ -59,7 +59,7 @@ const userSchema = new Schema(
 userSchema.pre("save",async function (next){
     if(!this.isModified("password"))return next();// password change na hua h toh kyu hi ese encrypt kre
 
-    this.password=bcrypt.hash(this.password, 10)
+    this.password=await bcrypt.hash(this.password, 10)
     next()
 })//it is a hook or middleware
 
